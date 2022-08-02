124b6a5179f349fad97e22c3155c9ed30e7fb76f
vishal5394 <vishaljadyal@gmail.com>
2022-08-02 15:20:04 +0530

    Added: Welcome to Linked List Program

---
+++ LinkedLists.java
@@ -0,0 +1,9 @@
+package com.BridgeLabz.LinkedList;
+
+
+public class LinkedLists{
+	public static void main(String[] args) {
+		System.out.println("Welcome to linled List Program" );
+	}
+}
+		      
